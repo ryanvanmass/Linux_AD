a229ad3f020b120816ebc3299a643e14e23e02fe
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2022-09-18 17:42:57 -0400

    Add Sudo group to sudoers file

--- Join.sh
+++ Join.sh
@@ -4,3 +4,5 @@ sudo apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common
 sudo realm join -U ryan $1
 
 sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
+
+sudo su -c 'echo "%sudo@rdvm.ca ALL=(ALL:ALL) ALL" >> /etc/sudoers'
