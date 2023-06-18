c7827e51677924ea04372e9bc5478cf68a060cae
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2023-06-18 16:50:21 -0400

    Update Domain Name

--- Join.sh
+++ Join.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y
 
-sudo realm join -U ryan $1
+sudo realm join -U ryan rdvm.local
 
 sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
 
-sudo su -c 'echo "%sudo@rdvm.ca ALL=(ALL:ALL) ALL" >> /etc/sudoers'
+sudo su -c 'echo "%sudo@rdvm.local ALL=(ALL:ALL) ALL" >> /etc/sudoers'
