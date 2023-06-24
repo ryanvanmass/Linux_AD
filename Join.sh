ba9c806b4db0959b2f3cb7f341221d745cabae48
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2023-06-24 16:14:34 -0400

    Add Import SSH keys from github

--- Join.sh
+++ Join.sh
@@ -6,8 +6,12 @@ sudo apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common
 sudo rm -f /etc/resolv.conf
 sudo ln -s /run/systemd/resolv/resolv.conf /etc/resolv.conf
 
+# Join Server to Domain
 sudo realm join -U ryan rdvm.local
 
 sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
 
 sudo su -c 'echo "%sudo@rdvm.local ALL=(ALL:ALL) ALL" >> /etc/sudoers'
+
+# Import SSH key to primary server admin account
+su ryan@rdvm.local -c "ssh-import-id-gh ryanvanmass"
