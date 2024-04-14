3cddec20b26bba50a26639b6772d5d906ae68e1c
Ryan Van Massenhoven <65045018+ryanvanmass@users.noreply.github.com>
2024-04-14 14:31:47 -0400

    Update Domain Name

--- Join.sh
+++ Join.sh
@@ -7,11 +7,11 @@ sudo rm -f /etc/resolv.conf
 sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # Join Server to Domain
-sudo realm join -U ryan rdvm.tech
+sudo realm join -U ryan rdvm.local
 
 sudo su -c 'echo "session optional  pam_mkhomedir.so skel=/etc/skel umask=077" >> /etc/pam.d/common-session'
 
-sudo su -c 'echo "%sudo@rdvm.tech ALL=(ALL:ALL) ALL" >> /etc/sudoers'
+sudo su -c 'echo "%sudo@rdvm.local ALL=(ALL:ALL) ALL" >> /etc/sudoers'
 
 # Import SSH key to primary server admin account
-su ryan@rdvm.tech -c "ssh-import-id-gh ryanvanmass"
+su ryan@rdvm.local -c "ssh-import-id-gh ryanvanmass"
