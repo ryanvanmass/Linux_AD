88c27e03f4e7859cd03ace4eecf768a01c19bbd4
Ryan Van Massenhoven <65045018+ryanvanmass@users.noreply.github.com>
2024-12-04 16:37:15 -0500

    Remove DNS fix for latest version of Ubuntu

--- Join.sh
+++ Join.sh
@@ -3,8 +3,8 @@
 sudo apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y
 
 # Fix DNS to recognize Active Directory controlled DNS
-sudo rm -f /etc/resolv.conf
-sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+# sudo rm -f /etc/resolv.conf
+# sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # Join Server to Domain
 sudo realm join -U ryan rdvm.local
