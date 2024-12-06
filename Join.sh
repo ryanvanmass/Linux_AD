8dd700d0aecbe1268b9b331f25849c53fc104005
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-12-05 22:22:58 -0500

    remove lagacy DNS Config

--- Join.sh
+++ Join.sh
@@ -2,9 +2,7 @@
 # Install Dependancies
 sudo apt install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit -y
 
-# Fix DNS to recognize Active Directory controlled DNS
-# sudo rm -f /etc/resolv.conf
-# sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
 
 # Join Server to Domain
 sudo realm join -U ryan rdvm.local
